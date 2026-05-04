fe0dfbc786af1a290598241e221414eb8e844638
ttazler <flouufedex@gmail.com>
2026-05-04 22:36:26 +0300

    added deletion of old data

--- dags/iceberg_spark.py
+++ dags/iceberg_spark.py
@@ -25,7 +25,7 @@ spark = SparkSession.builder \
     \
     .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
     .config("spark.hadoop.fs.s3a.access.key", тут пришлось хардкоднуть пароли, буду фиксить это, а то не запускалось нихуя) \
-    .config("spark.hadoop.fs.s3a.secret.key", тут пришлось хардкоднуть пароли, буду фиксить это, а то не запускалось нихуя) \
+    .config("spark.hadoop.fs.s3a.secret.key", тут пришлось хардкоднуть пароли, буду фиксить это, а то не запускалось нихуя ) \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
     \
@@ -42,3 +42,65 @@ df = spark.table("demo.db.transactions")
 df.show()
 
 spark.sql("SELECT * FROM demo.db.transactions.snapshots").show()
+
+new_data = [(1, 'Prostitutka', 777.0), (3,'Pidor', 300.0)]
+
+df_updatedb = spark.createDataFrame(new_data, ['id', 'user', 'amount'])
+df_updatedb.createOrReplaceTempView('updates')
+
+spark.sql("""
+    MERGE INTO demo.db.transactions t
+    USING updates u
+    ON t.id = u.id
+    WHEN MATCHED THEN UPDATE SET t.amount = u.amount
+    WHEN NOT MATCHED THEN INSERT *
+""")
+
+spark.sql("""
+    CALL demo.system.expire_snapshots(
+        table => 'db.transactions', 
+        retain_last => 1
+    )
+""").show
+
+spark.sql("""
+    CALL demo.system.remove_orphan_files(
+        table => 'db.transactions'
+    )
+""").show()
+
+spark.sql("""
+    CALL demo.system.rewrite_data_files(
+        table => 'db.transactions'
+    )
+""").show()
+
+print('данные готовы')
+spark.table('demo.db.transactions').orderBy('id').show()
+
+history_snap = spark.sql('SELECT snapshot_id FROM demo.db.transactions.snapshots').collect()
+perviy_snap = history_snap[0]['snapshot_id']
+print('олд данные')
+spark.read.option('snapshot-id', perviy_snap).table('demo.db.transactions').show()
+spark.sql('SELECT file_path, record_count FROM demo.db.transactions.files').show(truncate=False)
+
+print("список файлов таблицы")
+spark.sql("SELECT file_path, file_format, record_count, file_size_in_bytes FROM demo.db.transactions.files").show(truncate=False)
+
+# манифесты и физ файлы (это для просмотра что в наличии, оставлю навсякий)
+print("список манифестов")
+# spark.sql("SELECT path, length, added_files_count, existing_files_count FROM demo.db.transactions.manifests").show(truncate=False)
+spark.sql("""
+    SELECT 
+        path, 
+        length, 
+        added_data_files_count, 
+        existing_data_files_count 
+    FROM demo.db.transactions.manifests
+""").show(truncate=False)
+
+spark.sql('ALTER TABLE demo.db.transactions ADD PARTITION FIELD user')
+
+spark.sql("INSERT INTO demo.db.transactions VALUES (4, 'New_Guey', 5000.0)")
+
+spark.sql('SELECT file_path, partition FROM demo.db.transactions.files').show(truncate=False)
